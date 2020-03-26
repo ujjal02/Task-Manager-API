8457488b483aae21e9d3a28c4564777c2f73a2c2
ujc01 <30632545+ujc01@users.noreply.github.com>
2020-03-26 11:41:56 +0530

    Hiding Private data

--- src/index.js
+++ src/index.js
@@ -13,14 +13,3 @@ app.use(taskRouter)
 app.listen(port, () =>{
     console.log("Website is up on port ",port)
 })
-
-const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-    const token = jwt.sign({_id:'abc123'}, 'thisismynewcourse', {expiresIn: '15 seconds'})
-    console.log(token)
-    const data = jwt.verify(token, 'thisismynewcourse')
-    console.log(data)
-}
-
-myFunction()
--- src/models/user.js
+++ src/models/user.js
@@ -49,9 +49,11 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
-userSchema.methods.getPublicProfile = function(){
+userSchema.methods.toJSON = function(){
     const user = this
     const userObject = user.toObject()
+    delete userObject.password
+    delete userObject.tokens
     return userObject 
 }
 
