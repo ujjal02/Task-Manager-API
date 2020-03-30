a305dbccf148eebc44ace38bd9333796ed1a346f
ujc01 <30632545+ujc01@users.noreply.github.com>
2020-03-30 09:33:29 +0530

    Image upload

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const router = new express.Router()
 const User = require('../models/user')
 const auth = require('../middleware/auth')
@@ -87,6 +89,48 @@ router.patch('/users/me', auth, async (req, res) => {
         res.send(req.user)
     }catch(e){
         res.status(400).send(e)
+    }
+})
+
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            return cb(new Error('Please upload an image'))
+        }
+        cb(undefined, true)
+    }
+})
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) =>{
+    res.status(400).send({ error: error.message})
+})
+
+router.delete('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) =>{
+    res.status(400).send({ error: error.message})
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id)
+        if(!user || !user.avatar){
+            throw new Error()
+        }
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+    }catch(e){
+        req.status(404).send()
     }
 })
 
